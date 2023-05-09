b15457288c584044334f37116cc741f347db3132
Po52 <131165561+Po52@users.noreply.github.com>
2023-05-09 22:48:44 +0200

    Create gracz.h

---
+++ gracz.h
@@ -0,0 +1,20 @@
+//przeciązanie operatorów ':', '<' i '=='
+#ifndef GRACZ_H
+#define GRACZ_H
+
+#include <string>
+
+class Gracz {
+public:
+    Gracz();
+    Gracz(const std::string& nazwa, int liczba_punktow = 0);
+    std::string getNazwa() const;
+    int getLiczbaPunktow() const;
+    bool operator<(const Gracz& other) const;
+    bool operator==(const Gracz& other) const;
+private:
+    std::string m_nazwa;
+    int m_liczba_punktow;
+};
+
+#endif // GRACZ_H
